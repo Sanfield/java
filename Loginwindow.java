5d435353dca7e1708983feeefce911d6853ecffe
Sanfield <www.1042824123@qq.com>
2017-04-21 16:39:06 +0800

    SWING创建的桌面登陆窗口

---
+++ Loginwindow.java
@@ -0,0 +1,15 @@
+package com.sfjdbc.www;
+
+import javax.swing.JFrame;
+
+public class Loginwindow extends JFrame{
+public static void main(String[] args) {
+	
+	
+	   
+}
+	public Loginwindow() {
+		super();
+		setTitle("µÇÂ½");
+	}
+}
